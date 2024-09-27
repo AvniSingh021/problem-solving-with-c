6fcfa8ed410431d14abf29bc5df612e980baab28
AvniSingh021 <avnisingh2818@gmail.com>
2024-09-27 15:49:53 +0530

    Add files via upload

---
+++ swap.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int a=4,b=6,c=0;
+    printf("swap the number:\n");
+     c=a;
+     a=b;
+     b=c;
+     printf("after swaping the number %d\n",a);
+     printf("after swaping the number %d\n",b);
+     return 0;
+
+
+}
