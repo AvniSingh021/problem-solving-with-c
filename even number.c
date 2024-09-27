33de21f2e976a1882d8f988a1d37426fbec58a63
AvniSingh021 <avnisingh2818@gmail.com>
2024-09-27 16:30:41 +0530

    Add files via upload

---
+++ even number.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int a;
+    printf("enter the number to check weather it is even or odd:");
+    scanf("%d",&a);
+    if(a%2==2);
+    printf("odd number");
+    return 0;
+}
