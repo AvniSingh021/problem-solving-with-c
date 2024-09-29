b8411cdd5db4da3714ddb0ffb88c926f630e9275
AvniSingh021 <avnisingh2818@gmail.com>
2024-09-29 12:58:10 +0530

    Add files via upload

---
+++ star.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int i,k,n=4;
+    for(i=1;i<n;i++)
+    {
+        for(k=i; k <i; k++){
+        printf("*");
+    }
+    printf("\n");
+    }
+    return 0;
+
+    }
