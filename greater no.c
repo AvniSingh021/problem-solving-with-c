bfde71f1622c0145b14af881db46a1dbfab3bbca
AvniSingh021 <avnisingh2818@gmail.com>
2024-10-07 13:36:21 +0530

    Add files via upload

---
+++ greater no.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+	int a,b,c;
+	printf("enter the first no");
+	scanf("%d",&a);
+	printf("enter the second no:");
+	scanf("%d",&b);
+	printf("enter the third no:");
+	scanf("%d",&c);
+	if ((a>b)&&(a>c)){
+		printf("greater no is%d\n",a);
+	}
+	else if
+	(b>c){
+		printf("greater no is%d",b);
+	}
+	else{
+	printf("greater no is%d",c);
+}
+return 0;
+}
