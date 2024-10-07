23c2c219417f9de643d775dabf412a1189655e47
AvniSingh021 <avnisingh2818@gmail.com>
2024-10-07 13:55:48 +0530

    Add files via upload

---
+++ traffic.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+	char light;
+	printf("enter traffic light system:");
+	scanf("%c",&light);
+	if (light=='G')
+	{
+		printf("green-> Go");
+	}
+	else if (light=='Y'){
+		printf("yellow-> wait");
+	}
+	else if (light=='R'){
+		printf("red->stop");
+	}
+	else {
+		printf("invalid choice");
+	}
+	return 0;
+	
+}
