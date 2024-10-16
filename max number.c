ba8b35ff1a9f68e924aca777d3167a57c93f4e2c
AvniSingh021 <avnisingh2818@gmail.com>
2024-10-16 14:12:42 +0530

    Add files via upload

---
+++ max number.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int max();
+  int main(){
+	int a,b;
+	printf("enter the mix no:");
+	scanf("%d%d",&a,&b);
+	if(a>b){
+		printf("a is max number");
+	}
+	else
+	if("b is max number");
+	void main(){
+		max();
+	}
+}
