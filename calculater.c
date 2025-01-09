deffbe86c0cdc6a0f432d759280d27fdf53b1bc3
AvniSingh021 <avnisingh2818@gmail.com>
2025-01-09 20:44:16 +0530

    Add files via upload

---
+++ calculater.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int a;
+    scanf("%d",&a);
+    char ch;
+    scanf("%c",&ch);
+    int b;
+    scanf("%d",&b);
+    if(ch=='+')
+    printf("%d",a+b);
+    if(ch=='_')
+    printf("%d",a-b);
+    if(ch=='*')
+    printf("%d",a*b);
+    if(ch=='/')
+    printf("%f",(float)a/b);
+
+}
